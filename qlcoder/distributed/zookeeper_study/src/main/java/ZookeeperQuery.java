4b89a0cfad2d27df6fdbb681ce927083c53d3351
CHE Yulin <yche@cse.ust.hk>
2016-10-11 21:10:24 +0800

    add zookeeper and http header studyg

---
+++ qlcoder/distributed/zookeeper_study/src/main/java/ZookeeperQuery.java
@@ -0,0 +1,23 @@
+/**
+ * Created by cheyulin on 10/11/16.
+ */
+
+import java.io.IOException;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ZookeeperQuery {
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+        // 创建一个与服务器的连接
+        ZooKeeper zk = new ZooKeeper("121.201.8.217:2181", 60000, new Watcher() {
+            // 监控所有被触发的事件
+            public void process(WatchedEvent event) {
+                System.out.println("已经触发了" + event.getType() + "事件！");
+            }
+        });
+        System.out.println(new String(zk.getData("/qlcoder/zookeeper", false, null)));
+    }
+}
--- qlcoder/network/spider/download_specific_bytes.py
+++ qlcoder/network/spider/download_specific_bytes.py
@@ -1,7 +1,7 @@
 import urllib2
 
-req = urllib2.Request('http://www.python.org/')
-req.add_header('Range', 'bytes=0-20') # set the range, from 0byte to 19byte, 20bytes len
+req = urllib2.Request('http://www.qlcoder.com/download/hugefile')
+req.add_header('Range', 'bytes=12345678901-12345678999') # set the range, from 0byte to 19byte, 20bytes len
 res = urllib2.urlopen(req)
 
 data = res.read()
