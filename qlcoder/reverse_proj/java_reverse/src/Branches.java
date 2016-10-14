e9c72da05134026c78354b65eca3dfd64c4755ce
CHE Yulin <yche@cse.ust.hk>
2016-10-14 22:18:49 +0800

    update branches to exex

--- qlcoder/reverse_proj/java_reverse/src/Branches.java
+++ qlcoder/reverse_proj/java_reverse/src/Branches.java
@@ -2648,15 +2648,15 @@ public class Branches {
     }
 
     long eL(long var1) {
-        return var1 == 0L?145179942L:this.lC(var1 - 1L) ^ this.sy(var1 - 1L);
+        return 145179942L;
     }
 
     long lC(long var1) {
-        return var1 == 0L?145179942L:this.sy(var1 - 1L) ^ this.eL(var1 - 1L);
+        return 145179942L;
     }
 
     long sy(long var1) {
-        return var1 == 0L?145179942L:this.eL(var1 - 1L) ^ this.lC(var1 - 1L);
+        return 145179942L;
     }
 
     long oe(long var1) {
