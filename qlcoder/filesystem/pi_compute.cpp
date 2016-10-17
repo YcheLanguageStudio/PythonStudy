7983a4c15321ca3b1ad504ef7ffa7c24a18cf8a1
CHE Yulin <yche@cse.ust.hk>
2016-10-17 22:46:31 +0800

    update pi_compute

--- qlcoder/filesystem/pi_compute.cpp
+++ qlcoder/filesystem/pi_compute.cpp
@@ -12,8 +12,8 @@ int main() {
   struct stat st;
   fstat(fd, &st);
   long long whole_size = st.st_size;
-  // char compared_chars[] = {2, 7, 1, 8, 2, 8, 1, 8, 2, 8};
-  char compared_chars[] = {5, 9, 2, 6, 5, 3, 5, 8, 9, 8};
+  char compared_chars[] = {'2', '7', '1', '8', '2', '8', '1', '8', '2', '8'};
+  // char compared_chars[] = {'5', '9', '2', '6', '5', '3', '5', '8', '9', '8'};
 
   int count = 0;
   for (long long current_offset = 0; current_offset < whole_size;
@@ -25,13 +25,7 @@ int main() {
     }
     char *mmap_chars = (char *)mmap(NULL, mmap_size, PROT_READ, MAP_PRIVATE, fd,
                                     current_offset);
-    char three = '3';
-    cout << (int)mmap_chars[0];
-    cout << (mmap_chars[0] == three);
     for (long long i = 0; i < mmap_size - 10; i++) {
-      if (count < 20)
-        cout << mmap_chars[i];
-      count++;
       bool same = true;
       for (long long j = 0; j < 10; j++) {
         if (compared_chars[j] != mmap_chars[i + j]) {
@@ -40,7 +34,10 @@ int main() {
         }
       }
       if (same) {
-        cout << current_offset + i << endl;
+        cout << "cur:" << current_offset << endl;
+        cout << "cur:" << i << endl;
+        cout << "cur:" << current_offset + i << endl;
+        return 0;
       }
     }
     munmap(mmap_chars, mmap_size);
