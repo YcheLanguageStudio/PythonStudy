0b4abc8c25c48471cc77c97457e500b5c6e5d4e6
cheyulin <yche@cse.ust.hk>
2016-10-20 16:57:40 +0800

    pass the collaborative filtering q

--- qlcoder/data_mining/machine_learning/src/main/java/CollaborativeFiltering.java
+++ qlcoder/data_mining/machine_learning/src/main/java/CollaborativeFiltering.java
@@ -1,44 +1,36 @@
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
-import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.svd.ExpectationMaximizationSVDFactorizer;
 import org.apache.mahout.cf.taste.impl.recommender.svd.SVDRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
-import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
-import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.recommender.Recommender;
-import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Created by cheyulin on 10/19/16.
  */
+
 public class CollaborativeFiltering {
     public static void main(String[] args) throws IOException, TasteException {
 
         File modelFile = new File("train.txt");
 
         DataModel model = new FileDataModel(modelFile);
+        SVDRecommender svdrecommender = new SVDRecommender(model, new ExpectationMaximizationSVDFactorizer(model, 25, 100));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("test.txt"));
 
-        UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-
-        UserNeighborhood neighborhood = new NearestNUserNeighborhood(4, similarity, model);
-
-        Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-
-
-        SVDRecommender svdrecommender=new SVDRecommender(model,new ExpectationMaximizationSVDFactorizer(model,4,1000));
-
-        //给用户436821推荐1个物品
-        List<RecommendedItem> recommendations = recommender.recommend(436821, 1);
-
-        for (RecommendedItem recommendation : recommendations) {
-            System.out.println(recommendation);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            String item[] = line.split(",");
+            try {
+                System.out.print((int) Math.round(svdrecommender.estimatePreference(Long.parseLong(item[0]), Long.parseLong(item[1]))));
+            }
+            catch (Exception e){
+                System.out.print(3);
+            }
         }
     }
 
