1c66ad971840d9d9b483b7bc6b29fa66d0f806d5
cheyulin <yche@cse.ust.hk>
2016-10-28 15:02:45 +0800

    add flood fill finish markov study search

--- qlcoder/algorithms_data_structures/search/robot_path_search.py
+++ qlcoder/algorithms_data_structures/search/robot_path_search.py
@@ -2,17 +2,41 @@
 # coding:utf-8
 
 import urllib2
+import re
+import state_space
+import time
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36',
-    'Cookie': 'gr_user_id=3949c66a-56d6-49be-99e6-dc48710481cd; uuid=5811c0d121dd4; è¿é¢çç­æ¡æ¯oreo=eyJpdiI6Ik13RVwvTVlPd0pHMGFkWTJFdElndnhRPT0iLCJ2YWx1ZSI6ImJKUlMxaFA4a1lYV2J3b0NnN0ZUS1E9PSIsIm1hYyI6ImRjZTBjYjU4NDg5ZjNjYjk5OGNmZmE0MTk2MWU1NmIyMTA4NmViMmM2OWUxYTA3NjdhYjNiODdlOTNkMmFlNTYifQ%3D%3D; XSRF-TOKEN=eyJpdiI6IndsTFlJekoreDNFcTltQjJvQWYwNVE9PSIsInZhbHVlIjoiYis4SnV6SjliMWRhRVc0VnJzOVNCZGd6THREVENtOVR2ejV5QUQwRVNLdElFa2taeCs3cWJteVBseVdhdXJ2ZEI3SWRPd3pmNnRXSHNaeERBcVdaRHc9PSIsIm1hYyI6IjBhODM4MjczODQ3MmU3MDgzNWQ2OThiMGVmYzdkMjEzNWNhOWRiNmMxMjgzNDlkYjI1ZjBlYjMxYWU0NjVhZjcifQ%3D%3D; laravel_session=eyJpdiI6IkpPWGlaWXBmSWkzYVwvdE5NYTdUbWR3PT0iLCJ2YWx1ZSI6IndxZXN4MUdFc0pQTkFLZENWM1ZxMmxuMkRzODh0R0dUQ2liVVlJYlZkTmpodUVnR3g0YzR2bnRMNVBiR1hkMkhMdHN1RXFyY3NJME12VGtTWUFYa0pnPT0iLCJtYWMiOiI1NjAxODE1MjM5YjNiYzYyMDE5NjBlZTRlNWNlOWYxNzllY2Y5NjQ1ZThlNjI3N2MwOWI1YmY1ZWFmNDgyZTdlIn0%3D; gr_session_id_80dfd8ec4495dedb=00c7f621-140a-44d1-b53c-d356c37de177; Hm_lvt_420590b976ac0a82d0b82a80985a3b8a=1477531541,1477533371,1477535066,1477558482; Hm_lpvt_420590b976ac0a82d0b82a80985a3b8a=1477566715'
-}
+def execute_answer():
+    headers = {
+        'Cookie': 'gr_user_id=3949c66a-56d6-49be-99e6-dc48710481cd; uuid=5811c0d121dd4; è¿é¢çç­æ¡æ¯oreo=eyJpdiI6Ik13RVwvTVlPd0pHMGFkWTJFdElndnhRPT0iLCJ2YWx1ZSI6ImJKUlMxaFA4a1lYV2J3b0NnN0ZUS1E9PSIsIm1hYyI6ImRjZTBjYjU4NDg5ZjNjYjk5OGNmZmE0MTk2MWU1NmIyMTA4NmViMmM2OWUxYTA3NjdhYjNiODdlOTNkMmFlNTYifQ%3D%3D; XSRF-TOKEN=eyJpdiI6IndsTFlJekoreDNFcTltQjJvQWYwNVE9PSIsInZhbHVlIjoiYis4SnV6SjliMWRhRVc0VnJzOVNCZGd6THREVENtOVR2ejV5QUQwRVNLdElFa2taeCs3cWJteVBseVdhdXJ2ZEI3SWRPd3pmNnRXSHNaeERBcVdaRHc9PSIsIm1hYyI6IjBhODM4MjczODQ3MmU3MDgzNWQ2OThiMGVmYzdkMjEzNWNhOWRiNmMxMjgzNDlkYjI1ZjBlYjMxYWU0NjVhZjcifQ%3D%3D; laravel_session=eyJpdiI6IkpPWGlaWXBmSWkzYVwvdE5NYTdUbWR3PT0iLCJ2YWx1ZSI6IndxZXN4MUdFc0pQTkFLZENWM1ZxMmxuMkRzODh0R0dUQ2liVVlJYlZkTmpodUVnR3g0YzR2bnRMNVBiR1hkMkhMdHN1RXFyY3NJME12VGtTWUFYa0pnPT0iLCJtYWMiOiI1NjAxODE1MjM5YjNiYzYyMDE5NjBlZTRlNWNlOWYxNzllY2Y5NjQ1ZThlNjI3N2MwOWI1YmY1ZWFmNDgyZTdlIn0%3D; gr_session_id_80dfd8ec4495dedb=00c7f621-140a-44d1-b53c-d356c37de177; Hm_lvt_420590b976ac0a82d0b82a80985a3b8a=1477531541,1477533371,1477535066,1477558482; Hm_lpvt_420590b976ac0a82d0b82a80985a3b8a=1477566715'
+    }
 
-request = urllib2.Request('http://www.qlcoder.com/train/autocr?level=1')
-for header_element in headers:
-    print header_element, headers[header_element]
-    request.add_header(header_element, headers[header_element])
+    request = urllib2.Request('http://www.qlcoder.com/train/autocr', headers=headers)
 
-response = urllib2.urlopen(request, timeout=5)
-print response
+    response_stream = urllib2.urlopen(request, timeout=200)
+    html_str = response_stream.read()
+    my_list = html_str.split('\n')
+    for ele in my_list:
+        if re.match('level=.*[0-9]+', ele):
+            print ele
+            ele = ele[0:len(ele) - 4]
+            print ele
+            params = ele.split('&')
+            level = int(params[0].split('=')[1])
+            row_num = int(params[1].split('=')[1])
+            col_num = int(params[2].split('=')[1])
+            map_info = params[3].split('=')[1]
+            params_dict = {'level': level, 'row_num': row_num, 'col_num': col_num, 'map_info': map_info}
+    # time.sleep(5)
+    print params_dict
+    paste_str = state_space.get_answer_dict(params_dict['row_num'], params_dict['col_num'], params_dict['map_info'])
+    answer_url = 'http://www.qlcoder.com/train/crcheck?'
+    new_url = answer_url + paste_str
+    print new_url
+    request = urllib2.Request(new_url, headers=headers)
+    response_stream = urllib2.urlopen(request, timeout=200)
+    print response_stream.read()
+    # time.sleep(5)
 
+for i in range(1,100):
+    execute_answer()
--- qlcoder/algorithms_data_structures/search/state_space.py
+++ qlcoder/algorithms_data_structures/search/state_space.py
@@ -20,8 +20,8 @@ def dfs_search(init_row_idx, init_col_idx, marked_num, path_list, my_map_arr):
     max_col_idx = my_map_arr.shape[1] - 1
     max_row_idx = my_map_arr.shape[0] - 1
     whole_num = my_map_arr.shape[0] * my_map_arr.shape[1]
+
     if marked_num == whole_num:
-        print ''.join(path_list)
         return True
     else:
         # move left
@@ -45,7 +45,7 @@ def dfs_search(init_row_idx, init_col_idx, marked_num, path_list, my_map_arr):
                 my_changed_col_idx = init_col_idx
 
         # move right
-        if init_col_idx + 1 <= max_row_idx and my_map_arr[init_row_idx][init_col_idx + 1] == 0:
+        if init_col_idx + 1 <= max_col_idx and my_map_arr[init_row_idx][init_col_idx + 1] == 0:
             for idx in range(init_col_idx + 1, max_col_idx + 1):
                 if my_map_arr[init_row_idx][idx] == 1:
                     break
@@ -106,19 +106,29 @@ def dfs_search(init_row_idx, init_col_idx, marked_num, path_list, my_map_arr):
         return False
 
 
-if __name__ == '__main__':
-    my_map_info = '00000000000000000001000010000000000'
-    my_marked_num = len(filter(lambda x: x == '1', my_map_info))
-    my_row_num = 5
-    my_col_num = 7
+def get_answer_dict(my_row_num, my_col_num, my_map_info):
+    ret_dict = str()
     my_map_arr = init_map(my_row_num, my_col_num, my_map_info)
-
+    my_marked_num = len(filter(lambda x: x == '1', my_map_info)) + 1
     for i in range(0, len(my_map_info)):
         tmp_row_idx = i / my_col_num
         tmp_col_idx = i % my_col_num
-        print tmp_row_idx, tmp_col_idx
+        print tmp_row_idx,tmp_col_idx
         tmp_list = list()
         if my_map_arr[tmp_row_idx][tmp_col_idx] == 0:
-            if dfs_search(tmp_row_idx, tmp_col_idx, my_marked_num, tmp_list, copy.copy(my_map_arr)):
-                print tmp_list
+            new_map_arr = copy.deepcopy(my_map_arr)
+            new_map_arr[tmp_row_idx][tmp_col_idx] = 1
+            if dfs_search(tmp_row_idx, tmp_col_idx, my_marked_num, tmp_list, new_map_arr):
+                print 'find it'
+                ret_dict = 'x=' + str(tmp_row_idx + 1) + '&y=' + str(tmp_col_idx + 1) + '&path=' + ''.join(tmp_list)
                 break
+    return ret_dict
+
+
+if __name__ == '__main__':
+    my_map_str = '111000010000000100010010110000111100010100001100100110000001011000000100001111000000100001000000110011000011000000100000111100000000100000001110000010100001110000100000001001000000001000010001000001100000100001010011010001100010000111110001000000100000100000110001000001100011100101000101011100000001011101010000110000000001000000101000011011000000001000110000'
+    my_row_num_int = 18
+    my_col_num_int = 20
+
+    my_dict = get_answer_dict(my_row_num_int, my_col_num_int, my_map_str)
+    print my_dict
