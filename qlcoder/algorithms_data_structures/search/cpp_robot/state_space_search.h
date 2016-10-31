44a7da3d42b1f7a2f2ffc1a70c007500c9b6e3b4
CHE Yulin <yche@cse.ust.hk>
2016-10-31 22:10:49 +0800

    update robot cpp program still bugs

---
+++ qlcoder/algorithms_data_structures/search/cpp_robot/state_space_search.h
@@ -0,0 +1,49 @@
+#include <list>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+constexpr char MARK_CHAR = '2';
+constexpr char OCCUPY_CHAR = '1';
+constexpr char EMPTY_CHAR = '0';
+
+class RobotSolver {
+public:
+    using RobotMapType = vector<vector<char>>;
+
+    RobotSolver(int row_num, int col_num, string map_zero_one_str) : row_num_(row_num), col_num_(col_num),
+                                                                     marked_num_(0) {
+        for (auto my_ch:map_zero_one_str) {
+            if (my_ch == OCCUPY_CHAR)
+                marked_num_++;
+        }
+
+        robot_map_.resize(row_num_);
+        for (auto &col:robot_map_) {
+            col.resize(col_num_);
+        }
+        for (auto row_idx = 0; row_idx < row_num_; row_idx++) {
+            for (auto col_idx = 0; col_idx < col_num_; col_idx++) {
+                robot_map_[row_idx][col_idx] = map_zero_one_str[row_idx * col_num_ + col_idx];
+            }
+        }
+    }
+
+    string GetAnswerUrl();
+
+private:
+    int row_num_;
+    int col_num_;
+    int marked_num_;
+    RobotMapType robot_map_;
+
+
+    int GetConnectedCount(RobotMapType robot_map);
+
+    bool DepthFirstSearch(int init_row_idx, int init_col_idx, int marked_num, list<char>& path_list,
+                          RobotMapType &my_map_arr);
+
+};
--- qlcoder/algorithms_data_structures/search/state_space_search.cpp
+++
@@ -1,4 +0,0 @@
-#include "state_space_search.h"
-
-RobotSolver::DepthFirstSearch(int init_row, int init_col, int marked_num,
-                              list<char> path_list, RobotMapType &rebot_map) {}
