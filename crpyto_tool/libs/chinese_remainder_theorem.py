ae5542ccc1b17c7af5047b41a3dca53820af1938
cheyulin <yche@cse.ust.hk>
2016-12-07 18:48:05 +0800

    finish chinese remainder theorem

--- crpyto_tool/libs/chinese_remainder_theorem.py
+++ crpyto_tool/libs/chinese_remainder_theorem.py
@@ -1,6 +1,43 @@
-import extended_euclidean
+from extended_euclidean import ExtendedGcdEuclidean
+import copy
+
+
+def compute_multiplicative_inverse(modulo_num, another_num):
+    algo = ExtendedGcdEuclidean(modulo_num=modulo_num, another_num=another_num)
+    return algo.get_result()
 
 
 class ChineseRemainder:
     def __init__(self, pairwise_prime_nums):
-        self.little_m = pairwise_prime_nums
+        self.arr_m = pairwise_prime_nums
+        self.M = reduce(lambda left, right: left * right, pairwise_prime_nums, 1)
+        self.arr_M = map(lambda ele: self.M / ele, self.arr_m)
+        self.arr_mul_inverse_M = map(lambda ele: compute_multiplicative_inverse(ele, self.M / ele), self.arr_m)
+
+    def get_remainder_list(self, num):
+        return map(lambda ele: num % ele, self.arr_m)
+
+    def get_original_number(self, remainder_lst):
+        sum_num = 0
+        for i in range(len(remainder_lst)):
+            sum_num += remainder_lst[i] * self.arr_M[i] * self.arr_mul_inverse_M[i]
+        return sum_num % self.M
+
+
+def list_add(left_list, right_list):
+    new_list = copy.deepcopy(left_list)
+    if len(left_list) != len(right_list):
+        print 'error'
+        return None
+    else:
+        for i in range(len(right_list)):
+            new_list[i] += right_list[i]
+        return new_list
+
+
+if __name__ == '__main__':
+    remainder_solver = ChineseRemainder([37, 49])
+    print remainder_solver.get_remainder_list(973)
+    print remainder_solver.get_original_number(
+        list_add(remainder_solver.get_remainder_list(678), remainder_solver.get_remainder_list(973)))
+    print remainder_solver.get_original_number([23, 34])
