0834ca9dc923b912e99702304d60b5b782f08303
cheyulin <yche@cse.ust.hk>
2017-01-02 09:12:38 +0800

    add numpy array demo

--- study/third_party_library/numpy_usage/numpy_array_demo.py
+++ study/third_party_library/numpy_usage/numpy_array_demo.py
@@ -17,6 +17,14 @@ def demo_matrix():
     print np_matrix
 
 
+def demo_element_wise_op():
+    np_arr = np.array([1, 2, 3])
+    print np_arr
+    np_arr -= 1
+    print np_arr
+
+
 if __name__ == '__main__':
     demo_1d_arr()
     demo_matrix()
+    demo_element_wise_op()
