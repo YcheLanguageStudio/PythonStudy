a46117dfffc306c4534f1b7820db0a8e9299823a
cheyulin <yche@cse.ust.hk>
2017-02-17 16:33:37 +0800

    finish euler graph

--- bioinformatics/construct_eluer_graph.py
+++ bioinformatics/construct_eluer_graph.py
@@ -1,2 +1,68 @@
 import networkx as nx
 from process_sequence_data import *
+
+
+def construct_graph():
+    read_list = get_clone_list()
+    print read_list
+    edge_list = map(lambda ele: (ele[:-1], ele[1:],), read_list)
+    return nx.DiGraph(edge_list)
+
+
+def get_post_process_graph():
+    graph = construct_graph()
+    odd_v_list = filter(lambda v: graph.degree(v) % 2 == 1, graph.nodes())
+    if graph.out_degree(odd_v_list[0]) - graph.in_degree(odd_v_list[0]) < 0:
+        graph.add_edge(odd_v_list[0], odd_v_list[1])
+    else:
+        graph.add_edge(odd_v_list[1], odd_v_list[0])
+    return graph
+
+
+def check_connected_balanced(graph):
+    """
+    :type graph: nx.DiGraph
+    """
+    for v in graph.nodes():
+        print graph.in_degree(v), graph.out_degree(v)
+    sub_graph_list = nx.weakly_connected_component_subgraphs(graph, True)
+    for sub_graph in sub_graph_list:
+        print sub_graph.edges()
+
+
+def get_euler_path(graph):
+    """
+    :type graph: nx.DiGraph
+    """
+    start_v = graph.nodes()[0]
+    global_path, in_path_v_list = [start_v], {start_v}
+    covered_out_edge_dict = dict()
+    for v in graph.nodes():
+        covered_out_edge_dict[v] = []
+
+    while len(global_path) < graph.number_of_edges() + 1:
+        print 'find v with unvisited out edges'
+        for v in in_path_v_list:
+            if len(covered_out_edge_dict[v]) < graph.out_degree(v):
+                local_path = [v]
+                print 'expand'
+                last_v = local_path[-1]
+                while len(local_path) == 1 or last_v != v:
+                    last_v = local_path[-1]
+                    for src, dst in graph.out_edges(last_v):
+                        if dst not in covered_out_edge_dict[src]:
+                            covered_out_edge_dict[src].append(dst)
+                            local_path.append(dst)
+                            in_path_v_list.add(dst)
+                            break
+                idx = global_path.index(v)
+                global_path = global_path[0:idx] + local_path + global_path[idx + 1:]
+                break
+    return global_path
+
+
+if __name__ == '__main__':
+    graph = get_post_process_graph()
+    check_connected_balanced(graph)
+    v_list = get_euler_path(graph)
+    print 'super str:', v_list[0][0] + ''.join(map(lambda ele: ele[1:], v_list))
--- bioinformatics/process_sequence_data.py
+++ bioinformatics/process_sequence_data.py
@@ -5,21 +5,3 @@ def get_clone_list():
         return map(lambda ele: ele.strip(), ifs.readlines())
 
 
-def get_edge_list(clone_list):
-    prefix_list = map(lambda ele: ele[:len(ele) - 1], clone_list)
-    kmer_dict = {}
-    for i, prefix in enumerate(prefix_list):
-        if prefix not in kmer_dict:
-            kmer_dict[prefix] = []
-        kmer_dict[prefix].append(clone_list[i])
-    print 'kmer_dict:', kmer_dict
-
-    def form_edge_list(vertex):
-        suffix = vertex[1:]
-        if suffix not in kmer_dict:
-            return []
-        else:
-            tmp_list = filter(lambda ele: ele[0] != ele[1], zip([vertex] * len(vertex[1:]), kmer_dict[vertex[1:]]))
-            return tmp_list
-
-    return list(itertools.chain(*map(form_edge_list, clone_list)))
