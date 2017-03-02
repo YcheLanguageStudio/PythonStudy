98cadc9706622241107fcad66bb873c7c51f5dfa
cheyulin <yche@cse.ust.hk>
2017-03-02 11:48:25 +0800

    finish z algorithm homework

--- bioinformatics/string_matching/z_algorithm.py
+++ bioinformatics/string_matching/z_algorithm.py
@@ -1,3 +1,5 @@
+import itertools
+
 unique_character = '$'
 
 
@@ -29,21 +31,55 @@ def z_algorithm_detail(super_str, pat_size):
                 z_score_arr[k] = beta + match_num
                 if match_num > 0:
                     l, r = k, k + z_score_arr[k] - 1
-
-    return filter(lambda idx: z_score_arr[idx] == pat_size, range(len(super_str)))
+    return filter(lambda idx: z_score_arr[idx] == pat_size, range(len(super_str))), z_score_arr
 
 
 def search_pattern_str_z(pat, txt):
-    match_list = z_algorithm_detail(pat + unique_character + txt, len(pat))
-    return map(lambda idx: idx - len(pat + unique_character), match_list)
+    match_list, z_score_arr = z_algorithm_detail(pat + unique_character + txt, len(pat))
+    return map(lambda idx: idx - len(pat + unique_character), match_list), z_score_arr
 
 
 def search_pattern_str_naive(pat, txt):
     return filter(lambda i: txt[i:i + len(pat)] == pat, range(len(txt) - len(pat) + 1))
 
 
-if __name__ == '__main__':
+def test_cases():
     pat = 'aba'
     txt = 'bbabaxababay'
     print 'naive:', search_pattern_str_naive(pat, txt)
-    print 'z algorithm:', search_pattern_str_z(pat, txt)
+    print 'z algorithm:' + str(search_pattern_str_z(pat, txt)) + '\n'
+
+
+def solve_homework4():
+    pat = 'GC'
+    txt = 'GCTTGGCATA'
+    print 'naive:', search_pattern_str_naive(pat, txt)
+    print 'z algorithm:' + str(search_pattern_str_z(pat, txt)) + '\n'
+
+    alphabet = ['A', 'G', 'C', 'T']
+    wild_card_pat = 'G*'
+
+    def expand_wildcard_character(wildcard_str):
+        combinations = map(lambda my_tuple: ''.join(my_tuple),
+                           list(apply(itertools.product,
+                                      tuple(map(lambda ch: alphabet if ch == '*' else [ch], wildcard_str)))))
+        return combinations
+
+    z_score_arr_list = []
+    idx_list_set = set()
+    for pat in expand_wildcard_character(wild_card_pat):
+        print 'pat:', pat
+        print 'naive:', search_pattern_str_naive(pat, txt)
+        idx_list, z_score_arr = search_pattern_str_z(pat, txt)
+        print 'z algorithm:' + str((idx_list, z_score_arr)) + '\n'
+        z_score_arr_list.append(z_score_arr)
+        idx_list_set |= set(idx_list)
+
+    final_z_score_arr = reduce(lambda l, r: map(lambda my_pair: max(my_pair[0], my_pair[1]), zip(l, r)),
+                               z_score_arr_list, z_score_arr_list[0])
+    print idx_list_set, final_z_score_arr
+
+
+if __name__ == '__main__':
+    test_cases()
+    solve_homework4()
