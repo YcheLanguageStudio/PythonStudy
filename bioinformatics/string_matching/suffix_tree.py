d315268143f50a45acb5c6c7448d9b86037894a1
CheYulin <yche@cse.ust.hk>
2017-03-05 15:48:26 +0800

    update suffix tree impl remain bugs

--- bioinformatics/string_matching/suffix_tree.py
+++ bioinformatics/string_matching/suffix_tree.py
@@ -6,18 +6,23 @@ class EndIdx:
 class SuffixNode:
     class InEdgeLabel:
         def __init__(self, start_idx=-1, end_idx_ref=None):
+            """
+            :type end_idx_ref: EndIdx
+            """
             self.start_idx = start_idx
             self.end_idx_ref = end_idx_ref
 
         def edge_len(self):
-            assert isinstance(self.end_idx_ref, EndIdx)
-            return self.end_idx_ref.val - self.start_idx
+            return 0 if self.end_idx_ref is None else self.end_idx_ref.val - self.start_idx
 
-    def __init__(self, suffix_ref, start_idx=-1, end_idx_ref=None, suffix_idx=-1):
+    def __init__(self, suffix_link, start_idx=-1, end_idx_ref=None, suffix_idx=-1):
         self.children_dict = {}
-        self.suffix_ref = suffix_ref
+        self.suffix_link = suffix_link
         self.edge_label = SuffixNode.InEdgeLabel(start_idx, end_idx_ref)
         self.suffix_idx = suffix_idx
+
+    def is_leaf(self):
+        return len(self.children_dict) == 0
 
 
 class ActivePoint:
@@ -45,6 +50,7 @@ class UkknonenAlgorithm:
         self.end_idx = EndIdx()
         self.root_node = SuffixNode(None)
         self.active_point = ActivePoint(self.root_node, -1, 0)
+
         self.build_suffix_tree(whole_str)
 
     def build_suffix_tree(self, whole_str):
@@ -61,7 +67,7 @@ class UkknonenAlgorithm:
                     self.active_point.active_node.children_dict[edge_first_ch] = SuffixNode(self.root_node, pos,
                                                                                             self.end_idx)
                     if last_new_node is not None:
-                        last_new_node.suffix_ref = self.active_point.active_node
+                        last_new_node.suffix_link = self.active_point.active_node
                         last_new_node = None
                 else:
                     next_node = self.active_point.active_node.children_dict[edge_first_ch]
@@ -69,7 +75,7 @@ class UkknonenAlgorithm:
 
                     if whole_str[next_node.edge_label.start_idx + self.active_point.active_len] == whole_str[pos]:
                         if last_new_node is not None:
-                            last_new_node.suffix_ref = self.active_point.active_node
+                            last_new_node.suffix_link = self.active_point.active_node
                             last_new_node = None
 
                         self.active_point.active_len += 1
@@ -85,7 +91,7 @@ class UkknonenAlgorithm:
                     split_node.children_dict[whole_str[next_node.edge_label.start_idx]] = next_node
 
                     if last_new_node is not None:
-                        last_new_node.suffix_ref = split_node
+                        last_new_node.suffix_link = split_node
 
                     last_new_node = split_node
 
@@ -94,10 +100,26 @@ class UkknonenAlgorithm:
                     self.active_point.active_len -= 1
                     self.active_point.active_edge = pos - self.remaining_count + 1
                 elif self.active_point.active_node is not self.root_node:
-                    self.active_point.active_node = self.active_point.active_node.suffix_ref
+                    self.active_point.active_node = self.active_point.active_node.suffix_link
 
         for i in xrange(len(whole_str)):
             do_phase_extension(i)
+        self.mark_suffix_idx(len(whole_str))
+
+    def mark_suffix_idx(self, whole_str_len):
+        def dfs_detail(node_ref, cur_path_len):
+            """
+            :type node_ref: SuffixNode
+            """
+            if node_ref.is_leaf():
+                node_ref.suffix_idx = whole_str_len - cur_path_len
+                return
+            else:
+                for edge_first_ch in node_ref.children_dict:
+                    node = node_ref.children_dict[edge_first_ch]
+                    dfs_detail(node, cur_path_len + node_ref.edge_label.edge_len())
+
+        dfs_detail(self.root_node, 0)
 
 
 if __name__ == '__main__':
