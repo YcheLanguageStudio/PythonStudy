5f6dbcc8491758acf67cecb27aba529bb1a8a7c3
cheyulin <yche@cse.ust.hk>
2017-03-06 14:08:37 +0800

    refactor suffix tree and output

--- bioinformatics/string_matching/suffix_tree.py
+++ bioinformatics/string_matching/suffix_tree.py
@@ -1,30 +1,28 @@
-import copy
-
-
 class EndIdx:
     def __init__(self, val=-1):
         self.val = val
 
 
+class InEdgeLabel:
+    def __init__(self, start_idx=-1, end_idx_ref=None):
+        """
+        :type end_idx_ref: EndIdx
+        """
+        self.start_idx = start_idx
+        self.end_idx_ref = end_idx_ref
+
+    def sub_str(self, whole_str):
+        return '' if self.start_idx == -1 else whole_str[self.start_idx:self.end_idx_ref.val + 1]
+
+    def edge_len(self):
+        return 0 if self.start_idx == -1 else self.end_idx_ref.val - self.start_idx + 1
+
+
 class SuffixNode:
-    class InEdgeLabel:
-        def __init__(self, start_idx=-1, end_idx_ref=None):
-            """
-            :type end_idx_ref: EndIdx
-            """
-            self.start_idx = start_idx
-            self.end_idx_ref = end_idx_ref
-
-        def sub_str(self, whole_str):
-            return whole_str[self.start_idx:self.end_idx_ref.val + 1]
-
-        def edge_len(self):
-            return 0 if self.start_idx == -1 else self.end_idx_ref.val - self.start_idx + 1
-
     def __init__(self, suffix_link, start_idx=-1, end_idx_ref=None, suffix_idx=-1):
         self.children_dict = {}
         self.suffix_link = suffix_link
-        self.edge_label = SuffixNode.InEdgeLabel(start_idx, end_idx_ref)
+        self.edge_label = InEdgeLabel(start_idx, end_idx_ref)
         self.suffix_idx = suffix_idx
 
     def is_leaf(self):
@@ -53,86 +51,89 @@ class ActivePoint:
             return False
 
 
-class UkknonenAlgorithm:
+class SuffixTreeApp:
     def __init__(self, whole_str):
+        self.whole_str = whole_str
         self.remaining_count = 0
         self.end_idx = EndIdx()
         self.root_node = SuffixNode(None)
         self.active_point = ActivePoint(self.root_node, -1, 0)
-        self.build_suffix_tree(whole_str)
-        self.whole_str = whole_str
 
-    def build_suffix_tree(self, whole_str):
-        def do_phase_extension(pos):
-            self.remaining_count += 1
-            self.end_idx.val = pos
-            last_new_node = None
-            while self.remaining_count > 0:
-                if self.active_point.active_len == 0:
-                    self.active_point.active_edge = pos
+        def build_suffix_tree_via_ukkonen():
+            def do_phase_extension(pos):
+                self.remaining_count += 1
+                self.end_idx.val = pos
+                last_new_node = None
+                while self.remaining_count > 0:
+                    if self.active_point.active_len == 0:
+                        self.active_point.active_edge = pos
 
-                edge_first_ch = whole_str[self.active_point.active_edge]
-                if edge_first_ch not in self.active_point.active_node.children_dict:
-                    self.active_point.active_node.children_dict[edge_first_ch] = SuffixNode(self.root_node, pos,
-                                                                                            self.end_idx)
-                    if last_new_node is not None:
-                        last_new_node.suffix_link = self.active_point.active_node
-                        last_new_node = None
-                else:
-                    next_node = self.active_point.active_node.children_dict[edge_first_ch]
-                    if self.active_point.walk_down_if_possible(next_node):
-                        continue  # start from next node(the new active node)
-
-                    if whole_str[next_node.edge_label.start_idx + self.active_point.active_len] == whole_str[pos]:
+                    edge_first_ch = whole_str[self.active_point.active_edge]
+                    if edge_first_ch not in self.active_point.active_node.children_dict:
+                        self.active_point.active_node.children_dict[edge_first_ch] = SuffixNode(self.root_node, pos,
+                                                                                                self.end_idx)
                         if last_new_node is not None:
                             last_new_node.suffix_link = self.active_point.active_node
                             last_new_node = None
+                    else:
+                        next_node = self.active_point.active_node.children_dict[edge_first_ch]
+                        if self.active_point.walk_down_if_possible(next_node):
+                            continue  # start from next node(the new active node)
 
-                        self.active_point.active_len += 1
-                        break
+                        if whole_str[next_node.edge_label.start_idx + self.active_point.active_len] == whole_str[pos]:
+                            if last_new_node is not None:
+                                last_new_node.suffix_link = self.active_point.active_node
+                                last_new_node = None
 
-                    # in the middle of edge to split
-                    split_end_idx = EndIdx(next_node.edge_label.start_idx + self.active_point.active_len - 1)
-                    split_node = SuffixNode(self.root_node, next_node.edge_label.start_idx, split_end_idx)
-                    self.active_point.active_node.children_dict[whole_str[self.active_point.active_edge]] = split_node
+                            self.active_point.active_len += 1
+                            break
 
-                    split_node.children_dict[whole_str[pos]] = SuffixNode(self.root_node, pos, self.end_idx)
-                    next_node.edge_label.start_idx += self.active_point.active_len
-                    split_node.children_dict[whole_str[next_node.edge_label.start_idx]] = next_node
+                        # in the middle of edge to split
+                        split_end_idx = EndIdx(next_node.edge_label.start_idx + self.active_point.active_len - 1)
+                        split_node = SuffixNode(self.root_node, next_node.edge_label.start_idx, split_end_idx)
+                        self.active_point.active_node.children_dict[
+                            whole_str[self.active_point.active_edge]] = split_node
 
-                    if last_new_node is not None:
-                        last_new_node.suffix_link = split_node
+                        split_node.children_dict[whole_str[pos]] = SuffixNode(self.root_node, pos, self.end_idx)
+                        next_node.edge_label.start_idx += self.active_point.active_len
+                        split_node.children_dict[whole_str[next_node.edge_label.start_idx]] = next_node
 
-                    last_new_node = split_node
+                        if last_new_node is not None:
+                            last_new_node.suffix_link = split_node
 
-                self.remaining_count -= 1
-                if self.active_point.active_node is self.root_node and self.active_point.active_len > 0:
-                    self.active_point.active_len -= 1
-                    self.active_point.active_edge = pos - self.remaining_count + 1
-                elif self.active_point.active_node is not self.root_node:
-                    self.active_point.active_node = self.active_point.active_node.suffix_link
+                        last_new_node = split_node
 
-        def mark_suffix_idx(whole_str_len):
-            def dfs_detail(node_ref, cur_path_len):
-                """
-                :type node_ref: SuffixNode
-                """
-                if node_ref.is_leaf():
-                    node_ref.suffix_idx = whole_str_len - cur_path_len
-                    print node_ref.suffix_idx
-                else:
-                    for edge_first_ch in node_ref.children_dict:
-                        child_node = node_ref.children_dict[edge_first_ch]
-                        dfs_detail(child_node, cur_path_len + child_node.edge_label.edge_len())
+                    self.remaining_count -= 1
+                    if self.active_point.active_node is self.root_node and self.active_point.active_len > 0:
+                        self.active_point.active_len -= 1
+                        self.active_point.active_edge = pos - self.remaining_count + 1
+                    elif self.active_point.active_node is not self.root_node:
+                        self.active_point.active_node = self.active_point.active_node.suffix_link
 
-            dfs_detail(self.root_node, 0)
+            def mark_suffix_idx(whole_str_len):
+                def dfs_detail(node_ref, cur_path_len):
+                    """
+                    :type node_ref: SuffixNode
+                    """
+                    if node_ref.is_leaf():
+                        node_ref.suffix_idx = whole_str_len - cur_path_len
+                        print node_ref.suffix_idx
+                    else:
+                        for edge_first_ch in node_ref.children_dict:
+                            child_node = node_ref.children_dict[edge_first_ch]
+                            dfs_detail(child_node, cur_path_len + child_node.edge_label.edge_len())
 
-        # body of building suffix tree
-        for i in xrange(len(whole_str)):
-            do_phase_extension(i)
+                dfs_detail(self.root_node, 0)
 
-        print 'finish phase computation\n'
-        mark_suffix_idx(len(whole_str))
+            # body of building suffix tree
+            for i in xrange(len(whole_str)):
+                do_phase_extension(i)
+
+            print 'finish phase computation\n'
+            mark_suffix_idx(len(whole_str))
+            print 'finish suffix index marking\n'
+
+        build_suffix_tree_via_ukkonen()
 
     def print_suffix_tree(self):
         def dfs_detail(node_ref, prefix_str, node_level):
@@ -141,17 +142,19 @@ class UkknonenAlgorithm:
             :type prefix_str: str
             :type node_level: int
             """
+            blank_str = ''.join(["  "] * node_level)[:-1] + '|_' if node_level > 0 else 'root'
+            print '%-50s' % (blank_str + node_ref.edge_label.sub_str(self.whole_str),),
             if node_ref.is_leaf():
-                print ''.join([" "] * node_level) + prefix_str
+                print '(' + prefix_str + ',' + str(node_ref.suffix_idx) + ')'
             else:
+                print
                 for edge_first_ch in node_ref.children_dict:
                     child_node = node_ref.children_dict[edge_first_ch]
-                    dfs_detail(child_node, copy.deepcopy(prefix_str + child_node.edge_label.sub_str(self.whole_str)),
-                               node_level + 1)
+                    dfs_detail(child_node, prefix_str + child_node.edge_label.sub_str(self.whole_str), node_level + 1)
 
         dfs_detail(self.root_node, "", 0)
 
 
 if __name__ == '__main__':
-    algorithm = UkknonenAlgorithm("CTGCGT$GTCTGC#")
-    algorithm.print_suffix_tree()
+    app = SuffixTreeApp("CTGCGT$GTCTGC#")
+    app.print_suffix_tree()
---
+++ bioinformatics/string_matching/suffix_tree_output.md
@@ -0,0 +1,42 @@
+```zsh
+finish phase computation
+
+13
+12
+9
+0
+3
+8
+5
+10
+1
+11
+2
+7
+4
+6
+finish suffix index marking
+
+root                                              
+ |_#                                               (#,13)
+ |_C                                              
+   |_#                                             (C#,12)
+   |_TGC                                          
+     |_#                                           (CTGC#,9)
+     |_GT$GTCTGC#                                  (CTGCGT$GTCTGC#,0)
+   |_GT$GTCTGC#                                    (CGT$GTCTGC#,3)
+ |_T                                              
+   |_CTGC#                                         (TCTGC#,8)
+   |_$GTCTGC#                                      (T$GTCTGC#,5)
+   |_GC                                           
+     |_#                                           (TGC#,10)
+     |_GT$GTCTGC#                                  (TGCGT$GTCTGC#,1)
+ |_G                                              
+   |_C                                            
+     |_#                                           (GC#,11)
+     |_GT$GTCTGC#                                  (GCGT$GTCTGC#,2)
+   |_T                                            
+     |_CTGC#                                       (GTCTGC#,7)
+     |_$GTCTGC#                                    (GT$GTCTGC#,4)
+ |_$GTCTGC#                                        ($GTCTGC#,6)
+```
