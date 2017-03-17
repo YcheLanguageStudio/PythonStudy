71fabca4b2ae747bd1ef180be62041d87531b8e1
cheyulin <yche@cse.ust.hk>
2017-03-17 09:38:11 +0800

    add sol for discussion 7 problem 2

---
+++ bioinformatics/hypothesis_test/hypothesis_test.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    observed_list = [233, 385, 129]
+
+    p = float(observed_list[0] * 2 + observed_list[1]) / (sum(observed_list) * 2)
+    q = float(observed_list[2] * 2 + observed_list[1]) / (sum(observed_list) * 2)
+    expected_freq_list = [p ** 2, 2 * p * q, q ** 2]
+
+    observed_freq_list = map(lambda num: float(num) / 747, observed_list)
+    print 'observed:', observed_freq_list
+    print 'expected:', expected_freq_list
+
+    print sum(observed_list) * sum(
+        map(lambda pair: (pair[0] - pair[1]) ** 2 / pair[1], zip(observed_freq_list, expected_freq_list)))
